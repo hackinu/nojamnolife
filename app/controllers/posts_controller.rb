4a261f7a4636ac7f5e2f4f936b20590d2d800e9d
lim_zaem <dlarkdjs@hanmail.net>
2018-07-18 09:21:02 +0000

    devise gem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,14 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!
   # GET /posts
   # GET /posts.json
   def index
     @posts = Post.all
+    unless user_signed_in?
+      redirect_to "/users/sign_in"
+    end
+
   end
 
   # GET /posts/1
